70249ffb8844c3f20bfc63818d271c888a391839
Ricardo Torrellas <ricardo.torrejas@gmail.com>
2022-05-18 02:49:54 -0400

    feat

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import './../app.css';
 </script>
-
 <slot />
+<footer>
+	<a href="https://www.flaticon.com/free-icons/component" title="component icons">Component icons created by edt.im - Flaticon</a>
+</footer>
